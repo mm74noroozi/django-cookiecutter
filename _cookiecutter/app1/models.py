e128d479ef57a60653553fc910212d822bbf8dfe
Mohammad Noroozi <mm.74noroozi@gmail.com>
2023-11-02 00:48:59 +0330

    feat: basemodel + sample models

--- _cookiecutter/app1/models.py
+++ _cookiecutter/app1/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
-class Question(models.Model):
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract=True
+
+class Question(BaseModel):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
-class Answer(models.Model):
+class Answer(BaseModel):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     answer_text = models.CharField(max_length=200)
